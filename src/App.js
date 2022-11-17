60a64f21c870be3895e7d97f6d2632c6d79b3a87
Hyper-terminal <abhisharma001001001@gmail.com>
2022-11-17 12:12:01 +0530

    Added basic css and dynamic expense item value using props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+    const expenses = [
+        {
+            title: "Birthday Party",
+            amount: 5000,
+            date: new Date(2023, 0, 20).toUTCString(),
+            locationOfExpenditure: "Delhi",
+        },
+        {
+            title: "Car Insurance",
+            amount: 15000,
+            date: new Date(2023, 0, 25).toUTCString(),
+            locationOfExpenditure: "Gurgaon",
+        },
+        {
+            title: "Republic Day",
+            amount: 10000,
+            date: new Date(2023, 0, 26).toUTCString(),
+            locationOfExpenditure: "Delhi",
+        },
+    ];
+
     return (
         <div>
             <h2>Let's get started!</h2>
-            <ExpenseItem />
+            {expenses.map((expenseItem, index) => {
+                return (
+                    <ExpenseItem
+                        key={index}
+                        title={expenseItem.title}
+                        amount={expenseItem.amount}
+                        date={expenseItem.date}
+                        locationOfExpenditure={
+                            expenseItem.locationOfExpenditure
+                        }
+                    />
+                );
+            })}
         </div>
     );
 }
