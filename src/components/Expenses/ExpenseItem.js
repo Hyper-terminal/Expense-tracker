cb358f9631a6c4578d7a3784cc6450f288eda468
Abhi <abhi@pop-os.localdomain>
2022-11-18 12:42:08 +0530

    Added delete functionality using dom manipulation

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,29 @@
-import ExpenseDate from './ExpenseDate';
-import Card from '../UI/Card';
-import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const clickHandler = (e) => {
+    const parentNode = e.target.parentNode.parentNode;
+
+    const nodeToBeDeleted = e.target.parentNode;
+
+    parentNode.remove(nodeToBeDeleted);
+
+    console.log("Deleted");
+  };
+
   return (
-    <Card className='expense-item'>
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
+      <div className="expense-item__description">
         <h2>{props.title}</h2>
         <p>{props.locationOfExpenditure}</p>
-        <div className='expense-item__price'>${props.amount}</div>
+        <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>X</button>
       </div>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
