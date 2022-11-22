28468331d88221ce7e1e53672cab01d7c3c181c2
Hyper-terminal <abhisharma001001001@gmail.com>
2022-11-22 10:42:25 +0530

    Added chart element

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
 import "./Expenses.css";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState("2020");
@@ -19,6 +20,7 @@ const Expenses = (props) => {
     return (
         <div>
             <Card className="expenses">
+                <ExpensesChart expenses={filteredExpenses} />
                 <ExpensesFilter
                     selected={filteredYear}
                     onChangeFilter={filterChangeHandler}
